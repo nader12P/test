20702623bdc72d3a90ac055a514eaa74edcf509c
nader12P <101519141+nader12P@users.noreply.github.com>
2024-01-31 19:04:57 +0200

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -6,7 +6,7 @@ def getCommitID() {
     return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
 
-def buildDockerImage(DOCKER_REGISTRY, DOCKER_IMAGE) {
+def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
     script {
         def COMMIT_ID = getCommitID()
         sh 'docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} .'
