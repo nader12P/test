73ac64a6e8362b51ee7694bb6657bf5484029b55
nader12P <101519141+nader12P@users.noreply.github.com>
2024-01-31 21:51:10 +0200

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,15 +4,17 @@ def getCommitID() {
 
 def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
     script {
-        sh "echo '${DOCKER_IMAGE}'"
-        // def COMMIT_ID = getCommitID()
-        // sh "docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} ."
-        // return COMMIT_ID
+        def COMMIT_ID = getCommitID()
+        sh "docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} ."
+        return COMMIT_ID
     }
 }
 
-// def pushDockerImage() {
-//     sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
-//     sh "docker push ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
-//     sh "docker rmi ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
-// }
+def pushDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
+        def COMMIT_ID = getCommitID()
+        sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
+        sh "docker push ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID}"
+        sh "docker rmi ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID}"
+    }
+}
