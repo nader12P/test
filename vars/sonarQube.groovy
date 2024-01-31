1364548246572cb7d1e93445c798b78d3c0cbaad
nader12P <101519141+nader12P@users.noreply.github.com>
2024-01-31 17:59:59 +0200

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,6 +1,5 @@
-def call(string sonarToken, sonarHost, sonarProject, sonarHome) {
-    // this probably won't work, try to keep the withCredentials part int he jenkinsfile and see what happens
-    withCredentials([string(credentialsId: 'sonartoken', variable: 'SONAR_TOKEN')]) {
+def call(string sonarToken, SONAR_HOST, SONAR_PROJECT, SONAR_SCANNER_HOME) {
+    withCredentials([string(credentialsId: sonarToken, variable: 'SONAR_TOKEN')]) {
         sh "${SONAR_SCANNER_HOME}/bin/sonar-scanner -X -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONAR_TOKEN} -Dsonar.scm.provider=git  -Dsonar.java.binaries=build/classes" 
         sh 'rm -rf build'
     } 
