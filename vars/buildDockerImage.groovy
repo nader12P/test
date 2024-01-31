57c2cf0caecda788849df067334cfbd91a320901
nader12P <101519141+nader12P@users.noreply.github.com>
2024-01-31 19:03:22 +0200

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,14 +2,14 @@ def call() {
     // Define functions and variables here
 }
 
-def getCommitHash() {
+def getCommitID() {
     return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
 
 def buildDockerImage(DOCKER_REGISTRY, DOCKER_IMAGE) {
     script {
-        def COMMIT_HASH = getCommitHash()
-        sh 'docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER} .'
-        return COMMIT_HASH
+        def COMMIT_ID = getCommitID()
+        sh 'docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} .'
+        return COMMIT_ID
     }
 }
