edd790991e476609d15f742c0849f8f763f193ec
nader12P <101519141+nader12P@users.noreply.github.com>
2024-01-31 21:44:29 +0200

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,22 +1,17 @@
-def call() {
-
-    sh "echo 'Hello from call'"
+def getCommitID() {
+    return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
 
-// def getCommitID() {
-//     return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-// }
+def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
+    script {
+        def COMMIT_ID = getCommitID()
+        sh "docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} ."
+        return COMMIT_ID
+    }
+}
 
-// def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
-//     script {
-//         def COMMIT_ID = getCommitID()
-//         sh "docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} ."
-//         return COMMIT_ID
-//     }
-// }
-
-// def pushDockerImage() {
-//     sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
-//     sh "docker push ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
-//     sh "docker rmi ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
-// }
+def pushDockerImage() {
+    sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
+    sh "docker push ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
+    sh "docker rmi ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
+}
