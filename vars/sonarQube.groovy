32873e2ebcfd7ab2e0ca8b1cb155f80328659cc9
nader12P <101519141+nader12P@users.noreply.github.com>
2024-01-31 18:44:10 +0200

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,7 +1,8 @@
-def call(SONAR_HOST) {
-    sh 'echo \${SONAR_HOST}'
-    // withCredentials([string(credentialsId: 'sonartoken', variable: 'SONAR_TOKEN')]) {
-    //     sh "\${SONAR_SCANNER_HOME}/bin/sonar-scanner -X -Dsonar.projectKey=\${SONAR_PROJECT} -Dsonar.host.url=\${SONAR_HOST} -Dsonar.login=\${SONAR_TOKEN} -Dsonar.scm.provider=git  -Dsonar.java.binaries=build/classes" 
-    //     sh 'rm -rf build'
-    // }
+def call(SONAR_HOST, SONAR_PROJECT) {
+    def SONAR_SCANNER_HOME = tool "sonarqube"
+    
+    withCredentials([string(credentialsId: 'sonartoken', variable: 'SONAR_TOKEN')]) {
+        sh "\${SONAR_SCANNER_HOME}/bin/sonar-scanner -X -Dsonar.projectKey=\${SONAR_PROJECT} -Dsonar.host.url=\${SONAR_HOST} -Dsonar.login=\${SONAR_TOKEN} -Dsonar.scm.provider=git  -Dsonar.java.binaries=build/classes" 
+    }
+    sh 'rm -rf build'
 }
