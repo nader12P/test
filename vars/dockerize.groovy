a009507e7f9459faacb88c801482cb5f308455c4
nader12P <101519141+nader12P@users.noreply.github.com>
2024-02-01 07:59:07 +0200

    Update dockerize.groovy

--- vars/dockerize.groovy
+++ vars/dockerize.groovy
@@ -4,7 +4,7 @@ def getCommitID() {
     return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
 
-def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
+def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTERY) {
     script {
         def COMMIT_ID = getCommitID()
         sh "docker build -t ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} ."
@@ -12,7 +12,7 @@ def buildDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
     }
 }
 
-def pushDockerImage(DOCKER_IMAGE, DOCKER_REGISTRY) {
+def pushDockerImage(DOCKER_IMAGE, DOCKER_REGISTERY) {
     def COMMIT_ID = getCommitID()
     withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
         sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
