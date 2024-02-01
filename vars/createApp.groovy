92ea30e50ae9184d1e1d2f8a8135999b703e8954
nader12P <101519141+nader12P@users.noreply.github.com>
2024-02-01 08:07:38 +0200

    Update createApp.groovy

--- vars/createApp.groovy
+++ vars/createApp.groovy
@@ -1,10 +1,8 @@
-def call() {
-    // it's not going to work, figure it out
-    openshift.withCluster('ivolve-cluster') {
-        openshift.withProject('nader'){
-            sh "oc delete dc,svc,deploy,ingress,route \${DOCKER_IMAGE} || true"
-            sh "oc new-app ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:15"
-            sh "oc expose svc/${DOCKER_IMAGE}"
-        }
+def call(COMMIT_ID) {
+    withCredentials([file(credentialsId: 'openshift', variable: 'OPENSHIFT_SECRET')]) {
+        sh "oc project \${OPENSHIFT_PROJECT} --kubeconfig=$OPENSHIFT_SECRET"
+        sh "oc delete dc,svc,deploy,ingress,route \${DOCKER_IMAGE} --kubeconfig=$OPENSHIFT_SECRET|| true"
+        sh "oc new-app ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} --kubeconfig=$OPENSHIFT_SECRET"
+        sh "oc expose svc/${DOCKER_IMAGE} --kubeconfig=$OPENSHIFT_SECRET"
     }
 }
