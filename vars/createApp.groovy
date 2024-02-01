f2d8f27da9172e1966e939c3bc0e9e950a73a8eb
nader12P <101519141+nader12P@users.noreply.github.com>
2024-02-01 08:19:25 +0200

    Update createApp.groovy

--- vars/createApp.groovy
+++ vars/createApp.groovy
@@ -2,7 +2,7 @@ def call(COMMIT_ID, DOCKER_IMAGE) {
     withCredentials([file(credentialsId: 'openshift', variable: 'OPENSHIFT_SECRET')]) {
         sh "oc project \${OPENSHIFT_PROJECT} --kubeconfig=$OPENSHIFT_SECRET"
         sh "oc delete dc,svc,deploy,ingress,route ${DOCKER_IMAGE} --kubeconfig=$OPENSHIFT_SECRET|| true"
-        sh "oc new-app ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:${COMMIT_ID} --kubeconfig=$OPENSHIFT_SECRET"
+        sh "oc new-app ${DOCKER_REGISTERY}/${DOCKER_IMAGE}:20c9802 --kubeconfig=$OPENSHIFT_SECRET"
         sh "oc expose svc/${DOCKER_IMAGE} --kubeconfig=$OPENSHIFT_SECRET"
     }
 }
